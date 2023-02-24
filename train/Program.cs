610623894d8e4f0fc4263b07c1cf6583224d23df
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2023-02-25 00:26:44 +0300

    rew

--- train/Program.cs
+++ train/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Train
 {
@@ -6,67 +8,221 @@ namespace Train
     {
         static void Main()
         {
+            Console.Title = "Конструктор поездов";
+
+            DataBase data = new DataBase();
             TrainCreator creator = new TrainCreator();
-            Train train = creator.GetNewTrain();
-            Console.WriteLine(new string('-', 40));
+            creator.Work(data);
+        }
+    }
 
-            const string CommandSetDeparturePoint = "1";
-            const string CommandSetPointOfArrival = "2";
+    class DataBase
+    {
+        List<Train> _trains = new List<Train>();
 
-            bool isWork = true;
+        public void Add(Train train)
+        {
+            _trains.Add(train);
+        }
 
-            while(isWork)
+        public void ShowInfo()
+        {
+
+            if(_trains.Count > 0)
             {
-                train.ShowInfo();
-
-                switch(Console.ReadLine())
+                Console.SetCursorPosition(0, 24);
+                
+                Console.WriteLine("Поезда на линии");
+                
+                foreach(Train train in _trains)
                 {
-                    case CommandSetDeparturePoint:
-
-                    break;
-
-                    case CommandSetPointOfArrival:
-
-                    break;
+                    train.ShowInfo();
                 }
+
+                Console.SetCursorPosition(0, 0);
             }
         }
     }
 
-
     class TrainCreator
     {
+        private Direction _direction = new Direction("не", "задано");
+        private Wagon _wagon = new Wagon();
+        private int _tickets = 0;
+        private int _freePlaces = 0;
+        private int _wagons = 0;
 
 
-        public Train GetNewTrain()
+
+        public void Work(DataBase trainData)
         {
-            Direction direction = GetDirection();
-            Wagon wagons = null;
+            int nextStep = 0;
+            bool isNewTrain = true;
 
-            int tickets = SoldTickets();
-            Console.WriteLine($"количество проданных билетов {tickets}");
-            wagons = CreateWagons(tickets, wagons.OneWagonSeats);
-            int freePlaces = GetFreePlaces(wagons.WagonCount, tickets, wagons.OneWagonSeats);
-            // Train train = new Train(direction, wagons, freePlaces);
-            Train train = new Train(direction, wagons, freePlaces);
-            return train;
+            while(isNewTrain)
+            {
+                if(nextStep>0)
+                trainData.ShowInfo();
+                
+                ShowRealTimeInfo();
+
+                switch(nextStep)
+                {
+                    case 0:
+                    _direction = GetDirection();
+                    nextStep++;
+                    Console.WriteLine("Нажмите любую клавишу для продолжения ");
+                    break;
+
+                    case 1:
+                    _tickets = SoldTickets();
+                    nextStep++;
+                    Console.WriteLine("Нажмите любую клавишу для продажи билетов ");
+                    break;
+
+                    case 2:
+                    ShowSellingTickets(_tickets);
+                    _wagons = CreateWagons(_tickets, _wagon.Seats);
+                    _freePlaces = GetFreePlaces(_wagons, _tickets, _wagon.Seats);
+                    nextStep++;
+                    Console.WriteLine("Нажмите любую клавишу для создания вагонов");
+                    break;
+
+                    case 3:
+                    Train train = new Train(_direction, _wagons, _freePlaces);
+                    trainData.Add(train);
+                    nextStep++;
+                        Console.WriteLine("Нажмите любую клавишу для запуска поезда");
+                        break;
+
+                    case 4:
+                    isNewTrain = IsContinue();
+                    nextStep = 0;
+                        Console.WriteLine("Нажмите любую клавишу для продолжения ");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+            }
         }
 
         public Direction GetDirection()
         {
-            string departurePoint = "";
-            string arrivalPoint = "";
-
-            while(departurePoint == arrivalPoint)
+            string[] directions =
             {
-                Console.WriteLine("введите точку отправления ");
-                departurePoint = Console.ReadLine();
+                "Санкт-Петербург",
+                "Обухово","Колпино",
+                "Тосно",
+                "Чудово-московское",
+                "Малая Вишера",
+                "Окуловка",
+                "Бологое",
+                "Вышний Волочёк",
+                "Спирово","Тверь",
+                "Редкино*","Крюково",
+                "Лихоборы (бывш. НАТИ)",
+                "Москва (Ленинградский вокзал)"
+            };
 
-                Console.WriteLine("Введите точку прибытия");
-                arrivalPoint = Console.ReadLine();
+            string departurePoint = null;
+            string arrivalPoint = null;
+            bool isCorrectInput = false;
+
+            Console.WriteLine("Список актуальных маршрутов");
+
+            for(int i = 0; i < directions.Length; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {directions[i]}");
             }
 
+            while(isCorrectInput == false)
+            {
+                Console.Write("\nВведите номер точки отправления : ");
+                int index = ReadInt();
+
+                if(index >= 0 && index < directions.Length)
+                {
+                    departurePoint = directions[index];
+                }
+
+                Console.Write("\nВведите номер точки прибытия : ");
+                index = ReadInt();
+
+                if(index >= 0 && index < directions.Length)
+                {
+                    arrivalPoint = directions[index];
+                }
+
+                if(departurePoint == arrivalPoint)
+                {
+                    Console.WriteLine("Неверный Ввод!!! Точки отбытия и  прибытия не могут быть одинаковыми ");
+
+                }
+                else if(departurePoint == null || arrivalPoint == null)
+                {
+                    Console.WriteLine("Ошибка!!! Выбран не существующий маршрут");
+                }
+                else
+                {
+                    isCorrectInput = true;
+                }
+            }
             return new Direction(departurePoint, arrivalPoint);
+        }
+
+        private static int ReadInt()
+        {
+            int.TryParse(Console.ReadLine(), out int number);
+            int index = number - 1;
+            return index;
+        }
+
+        private void ShowSellingTickets(int tickets)
+        {
+            Console.CursorVisible = false;
+            Console.Clear();
+
+            for(int i = 0; i <= tickets; i++)
+            {
+                Console.SetCursorPosition(0, 0);
+
+                Console.WriteLine($"проданных билетов : {i}");
+                Thread.Sleep(40);
+            }
+
+            Console.CursorVisible = true;
+        }
+
+        private bool IsContinue()
+        {
+            const string CommandСonfirm = "yes";
+            const string CommanfDoNotСonfirm = "no";
+            Direction newDirection = new Direction("не", "задано");
+            Console.WriteLine($"продолжить работу {CommandСonfirm}/{CommanfDoNotСonfirm}");
+
+            if(Console.ReadLine() == CommanfDoNotСonfirm)
+                return false;
+            else
+            {
+                _tickets = 0;
+                _freePlaces = 0;
+                _wagons = 0;
+                _direction = newDirection;
+                return true;
+            }
+        }
+
+        private void ShowRealTimeInfo()
+        {
+            Console.WriteLine(new string('-', 30));
+            Console.Write("Направление : ");
+            _direction.Show();
+            Console.WriteLine($"\nПроданных билетов : {_tickets}");
+            Console.WriteLine($"Вагонов : {_wagons}");
+            Console.WriteLine($"Свободных мест в поезде : {_freePlaces}");
+            Console.WriteLine(new string('-', 30));
         }
 
         private int SoldTickets()
@@ -75,17 +231,16 @@ namespace Train
             return random.Next(120, 250);
         }
 
-        private Wagon CreateWagons(int tickets, int OneWagonSeats)
+        private int CreateWagons(int tickets, int OneWagonSeats)
         {
             int wagonsCount = tickets / OneWagonSeats;
 
             if(tickets % OneWagonSeats != 0)
             {
                 wagonsCount++;
-
             }
 
-            return new Wagon(wagonsCount);
+            return wagonsCount;
         }
 
         private int GetFreePlaces(int wagons, int tickets, int oneWagonSeats)
@@ -94,42 +249,24 @@ namespace Train
         }
     }
 
-    //class CartBuilder
-    //{
-
-    //}
-
     class Train
     {
-        private Wagon _wagonsCount;
+        private int _wagonsCount;
         private int _freePlaces;
         private Direction _direction;
 
-        public Train(string direction, Wagon wagonsCount, int freePlaces)
-        {
-            // _direction = direction;
-            _wagonsCount = wagonsCount;
-            _freePlaces = freePlaces;
-        }
-
-        public Train(Direction direction, Wagon wagonsCount, int freePlaces)
+        public Train(Direction direction, int wagonsCount, int freePlaces)
         {
             _direction = direction;
             _wagonsCount = wagonsCount;
             _freePlaces = freePlaces;
         }
 
-        //public void ShowInfo()
-        //{
-        //    Console.WriteLine($"| поезд {_departurePoint} - {_arrivalPoint} | количество вагонов {_wagonsCount}| свободных мест {_freePlaces}|");
-        //}
-
         public void ShowInfo()
         {
             Console.Write("|поезд ");
             _direction.Show();
-            Console.WriteLine($"| количество вагонов {_wagonsCount.WagonCount}| свободных мест {_freePlaces}|");
-
+            Console.WriteLine($"| количество вагонов {_wagonsCount}| свободных мест {_freePlaces}|");
         }
     }
 
@@ -144,42 +281,15 @@ namespace Train
             _arrival = arrival;
         }
 
-
-
         public void Show()
         {
             Console.Write($"{_departure} - {_arrival} ");
-
         }
-
     }
 
     class Wagon
     {
-        private int _wagonCount;
-
-        public Wagon(int wagonCount)
-        {
-            _wagonCount = wagonCount;
-        }
-
-        public int WagonCount => _wagonCount;
-        public int OneWagonSeats => 24;
+        public string Type => "бизнес";
+        public int Seats => 24;
     }
-
-    //class WaitingRoom
-    //{
-
-    //}
-
-    //class TrainCart
-    //{
-
-    //}
-
-    //class CartTypes
-    //{
-
-    //}
-
 }
